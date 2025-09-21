1f1d5b3d2099b6247dca6894648ffcbd8745e71c
KasymM3 <gaisin.k26@gmail.com>
2025-09-21 13:40:12 +0500

    Initial commit

---
+++ src/main/java/kz/example/Lab1/MyController.java
@@ -0,0 +1,27 @@
+package kz.example.Lab1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class MyController {
+
+    private List <Items> itemList = new ArrayList<>();
+
+    @GetMapping("/")
+    public String mainPage(Model model){
+        List<Items> itemList = new ArrayList<>();
+
+        // Добавляем 4 объекта Items
+        itemList.add(new Items( "Laptop", "Powerful gaming laptop", 1500.00));
+        itemList.add(new Items("Phone", "Latest smartphone with OLED display", 999.99));
+        itemList.add(new Items("Headphones", "Noise-cancelling wireless headphones", 199.50));
+        itemList.add(new Items( "Keyboard", "Mechanical keyboard with RGB lighting", 120.00));
+        model.addAttribute("items",itemList);
+        return "main_page";
+    }
+}
